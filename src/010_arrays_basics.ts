13ec001aade35a71bc7beb1820f357d81aa8745b
piyush-baniya <baniyapiyush0810@gmail.com>
2026-01-28 22:25:52 +0545

    array basics

---
+++ src/010_arrays_basics.ts
@@ -0,0 +1,6 @@
+const a11: number[] = [1, 2, 3]; // T[]
+const a22: Array<number> = [1, 2, 3]; // Array<T>
+
+const score = [10, 20, 30];
+// score.push("40"); // not possible to push strings
+const mix: (string | number)[] = [1, "2", 3, "4"];
